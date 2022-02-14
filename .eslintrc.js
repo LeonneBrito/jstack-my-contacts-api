7dbeb0ca7e764f490d631a2f525eab270318e193
Leonne Brito <britoleonne@gmail.com>
2022-02-14 20:43:20 -0300

    feat: Finished creating the API using in-memory database and repository pattern.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'class-methods-use-this': 'off',
+    'no-promise-executor-return': 'off',
+    'consistent-return': 'off',
+    'prefer-const': 'off',
+    'camelcase': 'off',
   },
 };
---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,82 @@
+const { v4: uuid } = require('uuid');
+
+let contacts = [
+  {
+    id: uuid(),
+    name: 'Leonne Brito',
+    email: 'contato@leonnebrito.com.br',
+    phone: '+55 (11) 99999-9999',
+    category_id: uuid(),
+  },
+  {
+    id: uuid(),
+    name: 'Matheus Silva',
+    email: 'contato@matheussilva.com.br',
+    phone: '+55 (11) 99999-9999',
+    category_id: uuid(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.email === email),
+    ));
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: uuid(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updateContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updateContact : contact
+      ));
+
+      resolve(updateContact);
+    }); 
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
